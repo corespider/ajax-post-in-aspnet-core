aa1a45e63d5a44c9324e3a0bbfe86df3c9f355d6
Your Name <53593343+CoreProgramm@users.noreply.github.com>
2021-09-15 21:59:19 +0530

    Add project files.

---
+++ CP.AjaxPost-ASPNetCore/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using CP.AjaxPost_ASPNetCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CP.AjaxPost_ASPNetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult AjaxMethod(string name)
+        {
+            CustomerModel person = new CustomerModel
+            {
+                Name = name,
+                DateTime = DateTime.Now.ToString()
+            };
+            return Json(person);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CP.AjaxPost-ASPNetCore/Models/CustomerModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CP.AjaxPost_ASPNetCore.Models
+{
+    public class CustomerModel
+    {
+        ///<summary>
+        /// Gets or sets Name.
+        ///</summary>
+        public string Name { get; set; }
+
+        ///<summary>
+        /// Gets or sets DateTime.
+        ///</summary>
+        public string DateTime { get; set; }
+    }
+}
